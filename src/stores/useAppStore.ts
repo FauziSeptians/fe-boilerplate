be766b5284803a44cebf35ebb590b5a2e8b9daf0
Muhammad Fauzi Septiana Putra <114633811+FauziSeptians@users.noreply.github.com>
2025-10-07 17:03:58 +0700

    feat: validators (#6)
    * feat: validators
    
    * feat: validators
    
    ---------
    
    Co-authored-by: muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,22 +1,22 @@
-import { User } from '@/types/user';
+import { UserTypes as User } from '@/types/user';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 type AppState = {
-      user: User | null;
-      isLoading: boolean;
-      setUser: (user:  User) => void;
-      setLoading: (loading: boolean) => void;
-}
+  user: User | null;
+  isLoading: boolean;
+  setUser: (user: User) => void;
+  setLoading: (loading: boolean) => void;
+};
 
 export const useAppStore = create<AppState>()(
-      persist(
-            (set) => ({
-                  user: null,
-                  isLoading: false,
-                  setUser: (user) => set({ user }),
-                  setLoading: (isLoading) => set({ isLoading }),
-            }),
-            { name: 'app-storage' }
-      )
+  persist(
+    (set) => ({
+      user: null,
+      isLoading: false,
+      setUser: (user) => set({ user }),
+      setLoading: (isLoading) => set({ isLoading }),
+    }),
+    { name: 'app-storage' }
+  )
 );
